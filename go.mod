cdb8204b7a27fdd783c0fff552e8edb32a0dae3f
huf0813 <student.harun0813@gmail.com>
2020-10-17 00:14:26 +0700

    fix(port): adding godotenv lib

--- go.mod
+++ go.mod
@@ -4,4 +4,5 @@ go 1.15
 
 require (
 	github.com/gorilla/mux v1.8.0
+	github.com/joho/godotenv v1.3.0
 )
--- main.go
+++ main.go
@@ -2,20 +2,21 @@ package main
 
 import (
 	"fmt"
+	"github.com/huf0813/pembukuan_tk/routes"
+	"github.com/joho/godotenv"
 	"log"
 	"net/http"
 	"os"
-
-	"github.com/huf0813/pembukuan_tk/routes"
 )
 
 var router routes.Route
 
 func main() {
-	port := os.Getenv("PORT")
-	if port == "" {
-		log.Fatal("$PORT must be set")
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
 	}
+
+	port := os.Getenv("PORT")
 
 	fmt.Printf("Running at port %s", ":"+port)
 	if err := http.ListenAndServe(":"+port, router.Routes()); err != nil {
